68b91a373cdc7c8f6dba0917afcd47050f5d0a1d
humanist96 <humanist96@gmail.com>
2023-12-21 17:22:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -78,21 +78,11 @@ if submit and question:
 
       top_hit_summary = response['hits']['hits'][0]['_source']['text'] # Store content of top hit for final step
 
-      messages=[
-          {"role": "system", "content": "You are a helpful assistant."},
-          {"role": "user", "content": "Translate the following text into english and answer the following question in Korean.:"
-           + question
-           + "by using the following text:"
-           + top_hit_summary},
-      ]
-      
-      print(messages[0])
-
       summary = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
               {"role": "system", "content": "You are a helpful assistant."},
-              {"role": "user", "content": "Translate the following text into english. and Answer the following question in Korean.:"
+              {"role": "user", "content": "Translate the following text into english and answer the following question in Korean.:"
                + question
                + "by using the following text:"
                + top_hit_summary},
